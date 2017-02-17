7a799ad4f54d9051b7463642e01eaac619abb32e
thetrumptoll <thetrumptoll@gmail.com>
2017-02-17 09:41:56 -0700

    adding elle mar-a-lago trip costs

--- data.js
+++ data.js
@@ -3,24 +3,35 @@ var data = [
         "date": "11/8/2016 to 01/20/2017",
         "title": "Trump Tower Security Costs from Election Day to Inauguration",
         "cost": 35000000,
-        "link": "https://www.nytimes.com/2016/12/05/nyregion/new-york-reimburse-trump-security-costs.html"
+        "link": "https://www.nytimes.com/2016/12/05/nyregion/new-york-reimburse-trump-security-costs.html",
+        "category": "security"
     },
     {
         "date": "2/3/2017",
         "title": "Eric Trump's business trip to Uruguay cost taxpayers $97,830 in hotel bills",
         "cost": 97830,
-        "link": "https://www.washingtonpost.com/business/economy/eric-trumps-trip-to-uruguay-cost-taxpayers-97830-in-hotel-bills/2017/02/03/ababd64e-e95c-11e6-bf6f-301b6b443624_story.html"
+        "link": "https://www.washingtonpost.com/business/economy/eric-trumps-trip-to-uruguay-cost-taxpayers-97830-in-hotel-bills/2017/02/03/ababd64e-e95c-11e6-bf6f-301b6b443624_story.html",
+        "category": "Family Business"
     },
     {
         "date": "2/3/2017",
-        "title": "Trump's Mar-a-Lago getaway could cost taxpayers more than $3 million",
-        "cost": 3000000,
-        "link": "http://www.politico.com/story/2017/02/trump-mar-lago-taxpayers-234562"
+        "title": "Superceded: Trump's Mar-a-Lago getaway could cost taxpayers more than $3 million",
+        "cost": 0,
+        "link": "http://www.politico.com/story/2017/02/trump-mar-lago-taxpayers-234562",
+        "category": "Vacations"
     },
     {
         "date": "2/8/2017",
         "title": "Unestimated: Department of Defense Seeks Space at Trump Tower",
         "cost": 0,
-        "link": "http://abcnews.go.com/Politics/department-defense-seeks-space-trump-tower/story?id=45342873"
+        "link": "http://abcnews.go.com/Politics/department-defense-seeks-space-trump-tower/story?id=45342873",
+        "category": "Real Estate"
+    },
+    {
+        "date": "2/17/2017",
+        "title": "Trumps 3 Trips to Mar-a-Lago Cost $10 Million and Your Taxes Are Paying For It",
+        "cost": 10000000,
+        "link": "http://www.elle.com/culture/career-politics/news/a43134/donald-trump-mar-a-lago-trips-10-million-taxpayers/",
+        "category": "Vacations"
     }
 ];
